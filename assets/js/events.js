66c6f678791d66489f5507276595d735d2030669
mayorgt_ <mikkystechs@gmail.com>
2024-06-18 11:34:15 +0100

    modified events page

---
+++ assets/js/events.js
@@ -0,0 +1,12 @@
+let next = document.querySelector(".new-next");
+let prev = document.querySelector(".new-prev");
+
+next.addEventListener("click", function () {
+    let items = document.querySelectorAll(".new-item");
+    document.querySelector(".new-slide").appendChild(items[0]);
+});
+
+prev.addEventListener("click", function () {
+    let items = document.querySelectorAll(".new-item");
+    document.querySelector(".new-slide").prepend(items[items.length - 1]);
+});
